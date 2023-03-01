4550ac7b7b8f9127f7532c886252af1eb3616d7e
naamafrank1 <naama0556772275@gmail.com>
2023-03-01 21:15:46 +0200

    conection string to mongodb atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+/* eslint-disable prettier/prettier */
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    //mongodb://localhost:27017/waze-project
+    //mongodb+srv://naama_frank-kod:VWSi2A2igrlqgFCw@cluster0.sddzqju.mongodb.net/waze-final-project?retryWrites=true&w=majority
+    ConfigModule.forRoot(),
+    
+   
+    MongooseModule.forRoot('mongodb+srv://Work_Schedule_app:dpBKp3FHsGfEHwmH@schedapp.shvvev6.mongodb.net/?retryWrites=true&w=majority'),  
+  
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+
+export class AppModule {
+
+}
